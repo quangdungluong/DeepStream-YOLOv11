68187557663f6118bc74b53afb3aeb6b818bce7b
Luong Quang Dung <luongquangdung00@gmail.com>
2024-09-30 22:33:22 +0700

    feat: add yolov11-pose

--- libs/install.sh
+++ libs/install.sh
@@ -6,4 +6,7 @@ echo "Building nvdsinfer_customparser_yolo bindings"
 cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo && cd $DIR
 
 echo "Building nvdsinfer_customparser_yolo_seg bindings"
-cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo_seg && cp -rv ./nvdsinfer_customparser_yolo_seg/libnvdsinfer_custom_impl_yolo_seg.so /app/libs && cd $DIR
+cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo_seg && cd $DIR
+
+echo "Building nvdsinfer_customparser_yolo_pose bindings"
+cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo_pose && cd $DIR
