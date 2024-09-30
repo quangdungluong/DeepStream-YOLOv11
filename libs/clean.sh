c7ad86d4ae074a646e015d5fc6d003c01126ef80
Luong Quang Dung <luongquangdung00@gmail.com>
2024-09-30 15:09:23 +0700

    feat: add yolo seg custom parser

--- libs/clean.sh
+++ libs/clean.sh
@@ -2,5 +2,8 @@
 DIR=$(pwd)
 echo $DIR
 
-echo "Building nvdsinfer_customparser_yolo bindings"
+echo "Clean nvdsinfer_customparser_yolo bindings"
 cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo clean && cd $DIR
+
+echo "Clean nvdsinfer_customparser_yolo_seg bindings"
+cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo_seg clean && cd $DIR
--- libs/install.sh
+++ libs/install.sh
@@ -4,3 +4,6 @@ echo $DIR
 
 echo "Building nvdsinfer_customparser_yolo bindings"
 cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo && cd $DIR
+
+echo "Building nvdsinfer_customparser_yolo_seg bindings"
+cd libs && CUDA_VER=$CUDA_VER make -C nvdsinfer_customparser_yolo_seg && cp -rv ./nvdsinfer_customparser_yolo_seg/libnvdsinfer_custom_impl_yolo_seg.so /app/libs && cd $DIR
